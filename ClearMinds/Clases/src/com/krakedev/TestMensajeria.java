870d2622482e09ccc45b4cf4b04862ea733072d5
BPaucar01 <118285902+BPaucar01@users.noreply.github.com>
2023-12-07 14:10:39 -0500

    Tarea 2
    Uso de métodos de las clases

---
+++ ClearMinds/Clases/src/com/krakedev/TestMensajeria.java
@@ -0,0 +1,16 @@
+package com.krakedev;
+
+public class TestMensajeria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mensajeria mensajeria = new Mensajeria();
+		
+		//Llamar a los métodos de la clase Mensajeria
+		
+		mensajeria.saluda();
+		mensajeria.saludarAmigos("Bryan", "Paucar");
+
+	}
+
+}
